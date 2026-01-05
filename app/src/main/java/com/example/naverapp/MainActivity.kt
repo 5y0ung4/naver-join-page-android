2ba7680109e3f365d9719da3a78dd571e65ecacc
5y0ung4 <qwerrr504@naver.com>
2026-01-05 23:55:56 +0900

    feat: add naver home UI

--- app/src/main/java/com/example/naverapp/MainActivity.kt
+++ app/src/main/java/com/example/naverapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.naverapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +12,14 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var birthMonth : Spinner; lateinit var sex : Spinner
+    lateinit var signup : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val home = Intent(this, HomeActivity::class.java)
+
+        signup = findViewById<Button>(R.id.signup)
 
         birthMonth = findViewById<Spinner>(R.id.spinnerMonth)
         sex = findViewById<Spinner>(R.id.sex)
@@ -36,5 +42,12 @@ class MainActivity : AppCompatActivity() {
         sexadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         sex.adapter = sexadapter
 
+        signup.setOnClickListener {
+            startActivity(home)
+        }
+
     }
+
+
+
 }
