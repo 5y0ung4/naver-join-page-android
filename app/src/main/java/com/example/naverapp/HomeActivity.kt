23280609aafa37be31a038a1866537eec840a3a2
5y0ung4 <qwerrr504@naver.com>
2026-01-06 22:50:39 +0900

    feat: add navigation drawer menu

--- app/src/main/java/com/example/naverapp/HomeActivity.kt
+++ app/src/main/java/com/example/naverapp/HomeActivity.kt
@@ -1,16 +1,26 @@
 package com.example.naverapp
 
+import android.content.ClipData
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var hamburger : ImageButton; lateinit var qrcode : ImageButton
     lateinit var searchBar : EditText;
+    lateinit var dl_main : DrawerLayout
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        dl_main = findViewById<DrawerLayout>(R.id.dl_main)
 
         qrcode = findViewById<ImageButton>(R.id.qrcode)
         qrcode.setOnClickListener {
@@ -19,8 +29,28 @@ class HomeActivity : AppCompatActivity() {
 
         hamburger = findViewById<ImageButton>(R.id.hamburger)
         hamburger.setOnClickListener {
-
+            dl_main.openDrawer(GravityCompat.START)
         }
+
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    // 홈 페이지 이동
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }
+                R.id.menu_setting -> {
+                    // 설정 페이지 이동
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                }
+            }
+
+            // 드로어 닫기
+            dl_main.closeDrawer(GravityCompat.START)
+            true
+        }
+
 
     }
 }
